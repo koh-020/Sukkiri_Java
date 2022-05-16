939561d9631fd2d62c4118786533b56995a2befa
koh-020 <97607232+koh-020@users.noreply.github.com>
2022-05-16 13:52:27 +0900

    add_practice4

---
+++ src/Practice/Practice4_1.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Practice4_1 {
+	int[] points = new int [4];
+	double[] weights = new double [5];
+	boolean[] answers = new boolean[3];
+	String[] names = new String[3];
+	
+
+}
---
+++ src/Practice/Practice4_2.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Practice4_2 {
+	public static void main(String[] args) {
+		int[] moneyList = { 121902, 8302, 551000 };
+		for (int money : moneyList) {
+			System.out.println(money);
+		}
+		System.out.println("");
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
+		
+	}
+
+}
