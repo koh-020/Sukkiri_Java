2b582def99bb29fd316a01e57d6ec712ba7723ec
koh-020 <97607232+koh-020@users.noreply.github.com>
2022-05-17 13:57:57 +0900

    practice_6

---
+++ src/practice/practice6/Main.java
@@ -0,0 +1,12 @@
+package practice.practice6;
+import practice.practice6.comment.Zenhan;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		practice.practice6.comment.Kouhan.callDeae();
+		practice.practice6.comment.Kouhan.showMondokoro();
+	}
+
+}
